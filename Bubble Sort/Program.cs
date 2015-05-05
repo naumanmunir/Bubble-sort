631b92233c12b1ca818aa7a9fa4e6dded4543b4b
nonabona <bloodo9@hotmail.com>
2015-05-05 14:33:20 -0400

    first

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unsorted = { 4, 2, 9, 20, 15, 8, 2, 4, 5, 16, 23 };
+            bool swapped;
+            int temp;
+            do
+            {
+                swapped = false;
+
+                for (int i = 0; i < unsorted.Length-1; i++)         //Length minus 1 is needed because array will become out of bounds and give a null exception
+                {
+                    if (unsorted[i] > unsorted[i + 1])
+                    {
+                        temp = unsorted[i + 1];
+                        unsorted[i + 1] = unsorted[i];
+                        unsorted[i] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            while (swapped);
+
+            for (int i = 0; i < unsorted.Length; i++)
+                Console.WriteLine(unsorted[i]+ " ");
+            
+        }
+    }
+}
